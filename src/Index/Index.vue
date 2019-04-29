80eee1aeab65aff3db60853b42491294e9476fca
waonngionn <air_mito.0622@icloud.com>
2019-04-29 10:39:53 +0900

    set up

---
+++ src/Index/Index.vue
@@ -0,0 +1,9 @@
+<template src="./Index.html"></template>
+<style lang="scss" src="./Index.scss"></style>
+
+<script>
+export default {
+  name: 'Index'
+}
+
+</script>
--- src/router/index.js
+++ src/router/index.js
@@ -1,6 +1,10 @@
 import Vue from 'vue'
 import Router from 'vue-router'
-import HelloWorld from '@/components/HelloWorld'
+import Index from '@/Index/Index'
+import About from '@/About/About'
+import Works from '@/Works/Works'
+import Contact from '@/Contact/Contact'
+import Sns from '@/Sns/Sns'
 
 Vue.use(Router)
 
@@ -8,8 +12,28 @@ export default new Router({
   routes: [
     {
       path: '/',
-      name: 'HelloWorld',
-      component: HelloWorld
+      name: 'Index',
+      component: Index
+    },
+    {
+      path: '/about',
+      name: 'About',
+      component: About
+    },
+    {
+      path: '/works',
+      name: 'Works',
+      component: Works
+    },
+    {
+      path: '/contact',
+      name: 'Contact',
+      component: Contact
+    },
+    {
+      path: '/sns',
+      name: 'Sns',
+      component: Sns
     }
   ]
 })
