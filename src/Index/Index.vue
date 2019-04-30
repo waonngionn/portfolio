85d1250af65911a53fe646f358fa3c2573971792
waonngionn <air_mito.0622@icloud.com>
2019-04-30 11:11:05 +0900

    openingを追加

--- src/Index/Index.vue
+++ src/Index/Index.vue
@@ -2,10 +2,12 @@
 <style lang="scss" src="./Index.scss"></style>
 
 <script>
+import Opening from './Opening/Opening.vue'
 import FloatWord from './FloatWord/FloatWord.vue'
 
 export default {
   components: {
+    Opening,
     FloatWord
   },
   name: 'Index'
---
+++ src/Index/Opening/Opening.vue
@@ -0,0 +1,8 @@
+<template src="./Opening.html"></template>
+<style lang="scss" src="./Opening.scss"></style>
+
+<script>
+export default {
+  name: 'Opening'
+}
+</script>
