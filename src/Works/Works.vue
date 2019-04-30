c825e5b1ae3a7e31322ce1bbce9b5c2fc28fc65b
waonngionn <air_mito.0622@icloud.com>
2019-04-30 19:19:52 +0900

    worksページ

--- src/Works/Works.vue
+++ src/Works/Works.vue
@@ -8,6 +8,29 @@ export default {
   components: {
     HeaderItem
   },
-  name: 'Works'
+  name: 'Works',
+  methods: {
+    gotoWork: function (index) {
+      // calunit
+      // background-color change
+      // utopia
+      const WorkLinkArr = [
+        'http://waonn.sakura.ne.jp/calunitfornit/',
+        'https://chrome.google.com/webstore/detail/change-background-color/jcodcbmhkoiolfcdhdjndolalehoonoo?hl=ja',
+        'https://chrome.google.com/webstore/detail/go-to-utopia/dakkdjbokdpjgihfcafddmeacoagidib?hl=ja'
+      ]
+      // window.location.href = WorkLinkArr[index]
+      open(WorkLinkArr[index], '_blank')
+    }
+  },
+  mounted: function () {
+    const target = document.getElementsByClassName('box_work')
+    const self = this
+    Object.keys(target).forEach(function (index) {
+      target[index].addEventListener('click', function () {
+        self.gotoWork(index)
+      }, false)
+    })
+  }
 }
 </script>
